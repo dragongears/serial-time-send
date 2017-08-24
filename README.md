924cd166ecfb3d5a30889feb5726fa011bf85f87
Art Dahm <adahm@pearlholding.com>
2017-08-24 14:35:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Serial Time Send
+
+A Node application to send the current time over a USB serial port. Useful for Arduino-type microcontroller projects that keep track of the time.
---
+++ index.js
@@ -0,0 +1,26 @@
+var SerialPort = require("serialport");
+
+var sp = new SerialPort("/dev/ttyACM0", {
+    baudRate: 115200
+});
+
+// Open serial port
+sp.on('open',function() {
+    var date = new Date();
+    var now = Math.floor((date.getTime() + date.getTimezoneOffset() * 60000) / 1000);
+
+    console.log('Serial port opened');
+
+    // Data from serial (should never happen)
+    sp.on('data', function(data) {
+        console.log('>>>>>', data);
+    });
+
+    sp.write('T' + now + '\r');
+    console.log('Date written: T' + now);
+
+    sp.close(function() {
+      console.log('Serial port closed');
+    });
+
+});
